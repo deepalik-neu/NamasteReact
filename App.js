ad34c49ef9aee456d39574bd215589558af65fc3
Deepali Kasture <deepalikasture@Deepalis-MacBook-Air-2.local>
2024-09-09 20:09:39 -0400

    epispde-01

---
+++ App.js
@@ -0,0 +1,21 @@
+
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("h2",{},"I am h2 tag")])
+);
+
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello World from react!"
+);
+//first we neeed to tell react add this to DOM and it is done through ReactDom
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//what is heading? it is just a JS object. it will return an object
+root.render(parent);
+//job of render: to take this object and put this into what is understood by document.
+// responsible to convert it to h1 tag and put it forward.
+
+//ReactElement(Object) => html(browser understandable)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # NamasteReact
+
+#What is JSX? 
+- It is not necessary to use it with React instead we can use React and React dom APIs.
+
+# React 
+- it is  JavaScript Library
+- We can use React just in small piece of page.
